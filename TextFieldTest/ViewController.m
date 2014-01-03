9b6cfaffb642460505fd3f1b40b6056a118387f2
hyunyoung158 <setarong158@gmail.com>
2014-01-03 18:15:04 +0900

    TextField delegate 연결을 실습했

--- TextFieldTest/ViewController.m
+++ TextFieldTest/ViewController.m
@@ -10,25 +10,55 @@
 
 @interface ViewController ()
 
-@property (weak, nonatomic) IBOutlet UITextField *textField;
+
 
 @end
 
 @implementation ViewController
 
-- (IBAction)dismissKeyboard:(id)sender {
-    [self.textField resignFirstResponder];
+//리턴키로 키보드감추기
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    NSLog(@"input: %@", textField.text);
+    [textField resignFirstResponder];
+    return YES;
+}
+
+//입력금지 2번
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+    if (2 == textField.tag) {
+        return NO;
+    }
+    return YES;
+}
+
+//z키 입력불가
+-(BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    if (3 == textField.tag && NSOrderedSame == [string compare:@"z" options:NSCaseInsensitiveSearch]) {
+        return NO;
+    }
+    return YES;
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
+    self.view.backgroundColor = [UIColor grayColor];
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(20, 205, 0, 0)];
+    label.text = @"Z입력금지";
+    label.textColor = [UIColor whiteColor];
+    label.backgroundColor = [UIColor clearColor];
+    [label sizeToFit];
+    [self.view addSubview:label];
+    
+    UITextField *textField = [[UITextField alloc] initWithFrame:CGRectMake(130, 200, 171, 31)];
+    textField.borderStyle = UITextBorderStyleRoundedRect;
+    textField.delegate = self; //viewController
+    textField.tag = 3;
+    
+    [self.view addSubview:textField];
 }
 
-- (void)viewDidAppear:(BOOL)animated{
-    [self.textField becomeFirstResponder];
-}
+
 
 - (void)didReceiveMemoryWarning
 {
